555705e2af1ea9d72d2ee4bc049d7b238f557c62
Joaquin <jechigueras@gmail.com>
2026-01-31 02:28:49 -0300

    Add phase space with marginal histograms for visualization

---
+++ phase_space_hist.py
@@ -0,0 +1,178 @@
+import random
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+from matplotlib.ticker import StrMethodFormatter
+
+#Calculate event radii
+def Radius_values(R,M_BH):
+    M_star = 1
+    #Stars and BH radii in au
+    R1 = R[0] * 0.004650467260962158
+    R2 = R[1] * 0.004650467260962158
+    R3 =  2*M_BH / 10065.320121972596**2
+    #DP radius
+    SMBH_Star_Coll_rad = R3
+    Star_Coll_rad = (R1+R2)*(1+1e-3)
+
+    Psi_star= (1.47 + np.exp((M_star - 0.669)/0.137) ) / (1. + 2.34 * np.exp((M_star - 0.669)/0.137) )
+    Psi_SMBH = (0.80 + 0.26 * (M_BH / 10**6)**0.5)
+    Rt_star = R1 * (M_BH/M_star)**(0.333333333333333333333)
+    #FTDE radius
+    Rt_Full_Star = Rt_star  * Psi_star* Psi_SMBH
+    #PTDE radius
+    Rt_Partial_Star = 2*Rt_star
+    return SMBH_Star_Coll_rad,Star_Coll_rad, Rt_Full_Star,Rt_Partial_Star
+
+
+#Calculate the max # of bins that have at least 5 points/bin
+def max_bins_count(data, min_per_bin=5):
+    data = np.asarray(data)
+    n = len(data)
+    if n < min_per_bin:
+        return 1
+    
+    max_possible_bins = n // min_per_bin
+    for bins in reversed(range(1, max_possible_bins + 1)):
+        counts, _ = np.histogram(data, bins=bins)
+        if np.all(counts >= min_per_bin):
+            return bins
+        
+    return 1
+
+EVENT_COLORS = {
+    'FTDE + No detectable': '#dc143c',     
+    'Se + Sc': '#393b79', 
+    'Double DP': '#20B2AA',
+    'Double FTDE': '#ffbb78',            
+    'Double single capture':'#8c564b',
+    'FTDE + PTDE': '#98df8a', 
+    'Single FTDE + No detectable': '#808080',  
+}
+
+#Creating dummy dataframe
+#------------------------------------------------------------------------------------------#
+
+#Number of samples
+N = 10000
+
+#Initial binary phase
+M1 = np.random.uniform(0,2*np.pi,size = N)
+M1_degrees = np.degrees(M1)
+
+#Initial impact parameter
+rp2 = np.random.uniform(0, 1.2, size = N)
+
+#Event names
+event_ID = (['DP + FTDE']*int(N/8) + ['FTDE + DP']*int(N/8) + 
+            ['Single ejection + Single capture']*int(N/8) + ['Double DP']*int(N/8) + 
+            ['Double FTDE']*int(N/8) + ['Double single capture']*int(N/8) +
+            ['FTDE + PTDE']*int(N/8) + ['Single FTDE + No detectable']*int(N/8))
+
+event_ID = random.sample(event_ID, len(event_ID))
+
+datos = {'M1': M1_degrees,
+         'rp2': rp2,
+         'event_ID': event_ID,
+         'Detectable': 'No'
+
+}
+#Creating the dataframe
+df = pd.DataFrame(datos)
+
+#------------------------------------------------------------------------------------------#
+
+#Exclude Double DP
+df = df[df['event_ID'] != 'Double DP'].copy()
+
+#Change name for shorter one
+df['event_ID'] = df['event_ID'].replace('Single ejection + Single capture', 'Se + Sc')
+
+#Masking to group two events and mark as detectable
+mask_ftde = df['event_ID'].isin(['DP + FTDE', 'FTDE + DP'])
+df.loc[mask_ftde, 'event_ID'] = 'FTDE + No detectable'
+df.loc[mask_ftde, 'Detectable'] = 'Yes'
+
+#Masking to mark events as detectables
+mask_detectable = df['event_ID'].isin(['Double FTDE', 'FTDE + PTDE', 'Single FTDE + No detectable'])
+df.loc[mask_detectable, 'Detectable'] = 'Yes'
+
+#Divide dataframe in two: detectable, no detectable
+df_detectable = df[df['Detectable'] == 'Yes'].copy()
+df_no_detectable = df[df['Detectable'] == 'No'].copy()
+
+#Constrain the 'M1' from 360° -> 180° since is simetrical 
+df_detectable['M1'] = df_detectable['M1'] % 180
+df_no_detectable['M1'] = df_no_detectable['M1'] % 180
+
+#Create the figure and the grid for the histograms
+fig = plt.figure(figsize=(8, 6))
+gs = gridspec.GridSpec(
+    3, 3,
+    width_ratios=[4, 0.8, 0.2],
+    height_ratios=[1, 4, 0.5],
+    wspace=0, hspace=0
+)
+
+ax_scatter = fig.add_subplot(gs[1, 0])
+xhist = fig.add_subplot(gs[0, 0])
+yhist = fig.add_subplot(gs[1, 1])
+
+
+order = 10
+
+#Scatter plot
+sns.scatterplot(
+    data=df_detectable, x='rp2', y='M1', hue='event_ID', edgecolor=None, palette=EVENT_COLORS,
+    s=10, ax=ax_scatter, zorder=order-2, legend=True, rasterized=True 
+)
+
+#Ploting event radii
+event_radius = Radius_values([1,1], 1e6)
+
+PTDE_radius = event_radius[3]
+FTDE_radius = event_radius[2]
+DP_radius = event_radius[0]
+
+for y, lbl, stl in zip(
+    [PTDE_radius, FTDE_radius, DP_radius],
+    ['PTDE radius', 'FTDE radius', 'DP radius'],
+    ['dashdot', 'dashed', 'solid']
+):
+    ax_scatter.axvline(x=y, color='black', linestyle=stl, linewidth=2, zorder=order-1, label=lbl)
+
+ax_scatter.set_xscale('log')
+ax_scatter.set_xlabel(r'Initial impact parameter $[au]$')
+ax_scatter.set_ylabel(r'Initial binary phase $[\degree]$')
+ax_scatter.set_yticks([0, 45, 90, 135, 180])
+ax_scatter.xaxis.set_major_formatter(StrMethodFormatter('{x:.5f}'))
+ax_scatter.grid(zorder=order-3)
+legend = ax_scatter.legend(loc='best', markerscale=3)
+legend.set_zorder(order)
+
+#Marginal histograms
+for name, group in df_detectable.groupby('event_ID'):
+    #Horizontal
+    xhist.hist(
+        group['rp2'], bins=max_bins_count(group['rp2']),
+        histtype='step', color=EVENT_COLORS.get(name, 'black'),
+        density=True
+    )
+    #Vertical
+    yhist.hist(
+        group['M1'], bins="auto", orientation='horizontal',
+        histtype='step', color=EVENT_COLORS.get(name, 'black'),
+        density=True
+    )
+
+xhist.set_xscale('log')
+xhist.set_xticks([])
+yhist.set_yticks([])
+yhist.set_ylim(ax_scatter.get_ylim())
+fig.suptitle(f'Detectable event distribution \nTotal: {len(df_detectable)}')
+
+plt.tight_layout()
+plt.show()
